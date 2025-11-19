5ffd4b61e850237ed7ea869bd61d0ddbbededce8
Nid silva <80358224+Ivoneide01@users.noreply.github.com>
2025-11-19 17:08:48 -0300

    img 5

--- js/app.js
+++ js/app.js
@@ -113,7 +113,13 @@ const produtos = [
         categoria: 'Tablet',
         preco: 4999.00,
         descricao: 'Tablet AMOLED 120Hz',
-        imagem: 'img/products/tab_s9.png'
+         imagem: 'https://m.media-amazon.com/images/I/417bx91cc8L._AC_SX679_.jpg',
+        imagens: [
+            'https://m.media-amazon.com/images/I/41A2TWFOOVL._AC_SX679_.jpg',
+            'https://m.media-amazon.com/images/I/41Co9eLcH1L._AC_SX679_.jpg',
+            'https://m.media-amazon.com/images/I/71r2uLmP3KL._AC_SX679_.jpg',
+            'https://m.media-amazon.com/images/I/3112F29KGdL._AC_SL1053_.jpg'
+        ]
     },
     {
         id: 6,
